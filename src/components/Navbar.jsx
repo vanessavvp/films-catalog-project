2d50946ec366700ef289a251a5af71bf92f1569e
vanessavillalba <vanessavillalbap@gmail.com>
2022-04-24 00:51:54 +0100

    TODO: Search component and add color config file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Button, ButtonGroup, Flex, Heading, Spacer, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <VStack p={10}>
+      <Flex w='100%'>
+        <Heading ml='10' as='h1' size='3xl'>Films catalog.</Heading>
+        <Spacer></Spacer>
+        <ButtonGroup variant='ghost' spacing='6'>
+          <Button colorScheme='purple' size='lg'>
+            <Link to='/'>Home</Link>
+          </Button>
+          <Button colorScheme='purple' size='lg'>
+            <Link to='/favorites'>Favorites</Link>
+          </Button>
+        </ButtonGroup>
+      </Flex>
+    </VStack>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+
+const Favorites = () => {
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+      </div>
+    </>
+  )
+}
+
+export default Favorites
